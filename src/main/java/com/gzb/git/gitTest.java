d2f518b311f374a9befe7ed5f98f40ab3db0c948
jerfalcon <jerfalcon@qq.com>
2021-10-05 15:42:43 +0800

    git version1

---
+++ src/main/java/com/gzb/git/gitTest.java
@@ -0,0 +1,13 @@
+package com.gzb.git;
+
+/**
+ * @author shkstart
+ * @create 2021-10-05 15:40
+ */
+public class gitTest {
+
+    public static void main(String[] args) {
+        System.out.println(" git version 1");
+    }
+
+}
